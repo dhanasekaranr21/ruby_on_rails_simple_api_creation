018b37aaa2fc3105568567edd8d5bc26eec4f281
dhanasekaranr21 <dhanasekaran.r@vakilsearch.com>
2021-07-13 15:45:44 +0530

    api-testing-and-code-fix

--- app/models/user_tracking.rb
+++ app/models/user_tracking.rb
@@ -1,2 +1,6 @@
 class UserTracking < ApplicationRecord
+    validates_presence_of :cookie
+    validates_presence_of :userAgent
+    validates_presence_of :fingerPrint
+    validates_presence_of :url
 end
